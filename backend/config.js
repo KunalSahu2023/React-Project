b6c2ed240124b49f97382a2c544f14ade45f16fb
Naman Jha <jhanaman786@gmail.com>
2024-04-28 17:44:41 +0530

    Add mongodb

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
-export const PORT = 3000;
+export const PORT = 5000;
+
+export const mongoDBURL = "mongodb+srv://" + process.env.MONGOUSER + ":" + process.env.MONGOPASSWORD + "@harvesttech.paz0jil.mongodb.net/?retryWrites=true&w=majority&appName=HarvestTech";
--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,38 @@
+import 'dotenv/config';
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import cors from "cors";
+import { User } from "./models/userModel.js";
 
 const app = express();
 
-app.get("/", (req, res)=>{
+app.use(express.json());
+
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type'],
+    })
+);
+
+app.get("/", (req, res) => {
     res.send("Hello");
 });
 
+app.post("/submit", (req, res) => {
+
+});
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log('App connected to database');
+    })
+    .catch((error) => {
+        console.log(error);
+    });
 app.listen(PORT, () => {
     console.log(`App is listening to port: ${PORT}`);
 });

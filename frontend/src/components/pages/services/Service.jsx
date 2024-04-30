683e6e29364aa2a679a651b7e903f13e72d0763b
Kunal Sahu <123799743+KunalSahu2023@users.noreply.github.com>
2024-04-30 22:28:39 +0530

    Update Service.jsx

--- frontend/src/components/pages/services/Service.jsx
+++ frontend/src/components/pages/services/Service.jsx
@@ -1,40 +1,46 @@
 import React from 'react';
-import Navbar from '../Navbar';
-import Footer from '../Footer';
+import './Services.css';
+import { Link } from 'react-router-dom';
+import Nav from './../navbar/Nav';
+import Footer from './../footer/Footer';
 
 const Service = () => {
   return (
     <>
-    <Navbar />
-<div id="service-container">
-    <h3>Our Services</h3>
+    <Nav />
+<div id="service-container" className='font'>
+    <h2>Our Services</h2>
 <div className="service-box">
     <div className="card">
         <i className="fa-sharp fa-solid fa-chalkboard-user"></i>
-        <h3>E-Learning Platform</h3>
+        <h3><Link to=''>E-Learning Platform</Link></h3>
     </div>
     <div className="card">
         <i className="fa-regular fa-solid fa-store"></i>
-        <h3>Marketplace Price</h3>
+        <h3><Link to=''>Marketplace Price</Link></h3>
     </div>
-    <div className="card">
+   <div className="card">
         <i className="fa-solid fa-indian-rupee-sign"></i>
-        <h3>Government Scheme & Subsidiaries</h3>
+        <h3>
+        <Link to='/govt scheme'>
+            Government Scheme & Subsidiaries
+    </Link>
+    </h3>
     </div>
 </div>
 
 <div className="service-box">
     <div className="card">
         <i className="fa-solid fa-globe"></i>
-        <h3>Localized Language Support</h3>
+        <h3><Link to=''>Localized Language Support</Link></h3>
     </div>
     <div className="card">
         <i className="fa-solid fa-sharp fa-cloud"></i>
-        <h3>Real-Time Weather Forecast</h3>
+        <h3><Link to=''>Real-Time Weather Forecas</Link>t</h3>
     </div>
     <div className="card">
         <i className="fa-solid fa-wheat-awn"></i>
-        <h3>Crop Information and Management</h3>
+        <h3><Link to=''>Crop Information and Management</Link></h3>
     </div>
 </div>
 </div>

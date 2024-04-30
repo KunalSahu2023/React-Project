15f327c057733f3d4bf1b1ac4f5cbe8dce98ef0e
Kunal Sahu <123799743+KunalSahu2023@users.noreply.github.com>
2024-04-30 22:17:44 +0530

    Update Api.jsx

--- frontend/src/components/pages/accordian/Api.jsx
+++ frontend/src/components/pages/accordian/Api.jsx
@@ -1,32 +1,27 @@
 export const questions = [
     {
         id: 1,
-        question: 'What is the virtual DOM?',
-        answer:'DOM stands for Document Object Model. The DOM represents an HTML document with a logical tree structure. Each branch of the tree ends in a node, and each node contains objects.'
+        question: 'What is Harvest Tech ?',
+        answer:'Harvest Tech is a project aimed at leveraging technology to enhance various aspects of the harvesting process in agriculture..'
     },
     {
         id: 2,
-        question: 'How do you create a React app?',
-        answer:'Install the create-react-app package using the command prompt or terminal.'
+        question: 'What are the main objectives of Harvest Tech?',
+        answer:'The primary objectives of Harvest Tech include improving efficiency, productivity, and sustainability in agricultural harvesting practices through the integration of innovative technologies.'
     },
     {
         id: 3,
-        question: 'What is an event in React?',
-        answer:'An event is an action that a user or system may trigger, such as pressing a key, a mouse click, etc.'
+        question: 'How does Harvest Tech benefit farmers?',
+        answer:'Harvest Tech offers several benefits to farmers, including increased crop yield, reduced labor costs, optimized resource utilization (such as water and fertilizers), enhanced decision-making through data-driven insights, and improved overall efficiency in the harvesting process.'
     },
     {
         id: 4,
-        question: ' What are synthetic events in React?',
-        answer:"Synthetic events combine the response of different browser's native events into one API, ensuring that the events are consistent across different browsers."
+        question: ' How can farmers access and implement Harvest Tech on their farms?',
+        answer:"Farmers can access Harvest Tech through a subscription-based model or by purchasing the necessary hardware and software components. Implementation support and training are provided to ensure a smooth transition and optimal utilization of the technology"
     },
     {
         id: 5,
-        question: 'What are the components in React?',
-        answer:'Components are the building blocks of any React application, and a single app usually consists of multiple components. A component is essentially a piece of the user interface. It splits the user interface into independent, reusable parts that can be processed separately.'
-    },
-    {
-        id: 6,
-        question: 'What are the components in Vinod?',
-        answer:'Components are the building blocks of any React application, and a single app usually consists of multiple components. A component is essentially a piece of the user interface. It splits the user interface into independent, reusable parts that can be processed separately.'
+        question: 'What support and resources are available for users of Harvest Tech??',
+        answer:'Harvest Tech provides ongoing support, training, and updates to users, along with access to a knowledge base, troubleshooting resources, and a dedicated customer service team to address any issues or inquiries.'
     }
 ]

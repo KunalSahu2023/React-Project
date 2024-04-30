ac7b6251b12ebdd734ed2f768fe915e87783b40b
Kunal Sahu <123799743+KunalSahu2023@users.noreply.github.com>
2024-04-30 22:40:09 +0530

    Update Hero.jsx

--- frontend/src/components/pages/Hero.jsx
+++ frontend/src/components/pages/Hero.jsx
@@ -1,22 +1,27 @@
 import React from 'react';
-import Navbar from './Navbar';
-import Footer from './Footer';
-import Answers from './accordian/Answers';
+import './Hero.css';
+import Navbar from './../navbar/Navbar';
+import Questions from './../accordian/Questions';
+import Footer from './../footer/Footer';
 
-const Hero = () => {
+function Hero() {
+
   return (
    <>
    <Navbar />
-   <div id="home-container">
+  <div id="home-container">
     <div class="content-box">
-      <video src='' />
-        <h1>Welcome In Harvest Tech</h1>
+        <h1>Welcome To Harvest Tech</h1>
         <p>Growing Smarter, Harvesting Better.</p>
+        
        </div>
        <div class="image-box-">
+     <video
+     src=''
+     controls muted loop />
        </div>
     </div>
-    <Answers />
+    <Questions />
     <Footer />
    </>
   )

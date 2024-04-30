550d2241154950bbbf1d5c8f54ee78d9b0ad8377
Kunal Sahu <123799743+KunalSahu2023@users.noreply.github.com>
2024-04-30 22:44:15 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,33 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
-import Hero from './components/pages/Hero';
-import Account from './components/pages/Account';
-import About from './components/pages/About';
-import Contact from './components/pages/Contact';
-import Deshboard from './components/pages/dashboard/Deshboard';
+import Hero from './components/pages/hero/Hero';
+import About from './components/pages/about/About';
 import Service from './components/pages/services/Service';
+import Contact from './components/pages/contact/Contact';
 import Scheme from './components/pages/services/Scheme';
+import Deshboard from './components/pages/dashboard/Dashboard';
 import DashboardBlog from './components/pages/dashboard/DashboardBlog';
 import Weather from './components/pages/dashboard/Weather';
 import Profile from './components/pages/dashboard/Profile';
 import Event from './components/pages/dashboard/Event';
-
+import Login from './components/pages/account/Login';
+import Register from './components/pages/account/Register';
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
      <Route path='/' element={<Hero />} />
       <Route path='about' element={<About />} />
-      <Route path='service' element={<Service/>} />
+      <Route path='service' element={<Service />} />
       <Route path='contact' element={<Contact />} />
-      <Route path='account' element={<Account />} />
       <Route path='govt scheme' element={<Scheme />} />
       <Route path='/dashboard' element={<Deshboard />} />
       <Route path='/dashboard/blog' element={<DashboardBlog />} />
       <Route path='/dashboard/weather' element={<Weather/>} />
       <Route path='/dashboard/profile' element={<Profile />} />
       <Route path='/dashboard/event' element={<Event />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/login' element={<Login />} />
+
       </>
   )
 )
